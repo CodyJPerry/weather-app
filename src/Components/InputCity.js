6e4c77432ba38f15f9f18309e39eafc4662a1cb0
Cody Perry <perrysdevelopment@gmail.com>
2023-06-27 20:48:01 -0400

    Add starter files

---
+++ src/Components/InputCity.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const InputCity = () => {
+  return <h1>InputCity</h1>;
+}
+
+export default InputCity;
---
+++ src/Components/ShowWeather.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ShowWeather = () => {
+  return <h1>ShowWeather</h1>;
+}
+
+export default ShowWeather;
